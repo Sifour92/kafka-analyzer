4e5f4cbf0deff00d15d558e5ad79d5a24eff9d26
Aleksandr Bernatovich <sagder91@gmail.com>
2024-01-03 09:48:07 +0300

    add data-analyzer-microservice

---
+++ src/main/java/com/example/dataanalysermicroservice/model/Data.java
@@ -0,0 +1,34 @@
+package com.example.dataanalysermicroservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "data")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Data {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long sensorId;
+    private LocalDateTime timestamp;
+    private double measurement;
+
+
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
+    private MeasurementType measurementType;
+
+    public enum MeasurementType {
+        TEMPERATURE,
+        VOLTAGE,
+        POWER
+    }
+}
---
+++ src/main/java/com/example/dataanalysermicroservice/repository/DataRepository.java
@@ -0,0 +1,8 @@
+package com.example.dataanalysermicroservice.repository;
+
+import com.example.dataanalysermicroservice.model.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DataRepository extends JpaRepository<Data,Long> {
+
+}
